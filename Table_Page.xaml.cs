7a6c43c265c650fef9d5a5c2ff024c6beac246ec
DavidMyrseth <153905309+DavidMyrseth@users.noreply.github.com>
2025-04-03 10:05:29 +0300

    Add files via upload

--- Table_Page.xaml.cs
+++ Table_Page.xaml.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using Microsoft.Maui.Controls;
 
 using System.Collections.Generic;
---
+++ Telefon.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MauiApp1
+{
+    public class Telefon
+    {
+        public string Nimetus { get; set; } // Название телефона
+        public string Tootja { get; set; }  // Производитель
+        public int Hind { get; set; }       // Цена
+        public string Pilt { get; set; }    // Путь к изображению
+    }
+}
